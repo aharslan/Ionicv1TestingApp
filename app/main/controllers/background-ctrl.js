d24cdf7b49f8a684f29b6a0cb695d983aa8023f9
Ahmad Arslan <arslan0157@gmail.com>
2017-02-01 14:50:21 +0500

    logging socket ready state

--- app/main/controllers/background-ctrl.js
+++ app/main/controllers/background-ctrl.js
@@ -1,13 +1,16 @@
 'use strict';
 angular.module('main')
-.controller('BackgroundCtrl', function ($log, $localStorage, $scope, $timeout) {
+.controller('BackgroundCtrl', function ($log, $localStorage, $scope, $timeout, Socket) {
 
+  Socket;
+  $scope.logText = '';
   $scope.count = 0;
   $localStorage.count = 0;
 
   var updateCounter = function () {
     $localStorage.count ++;
     $scope.count = $localStorage.count;
+    $scope.logText += Socket.returnsocket().readyState + '\n';
     $timeout(updateCounter, 1000);
   };
   updateCounter();
@@ -15,35 +18,7 @@ angular.module('main')
   $scope.reset = function () {
     $scope.count = 0;
     $localStorage.count = 0;
+    $scope.logText = '';
   };
 
 });
-
-// .factory('StorageService', function ($localStorage) {
-//   var _getAll = function () {
-//     return $localStorage.things;
-//   };
-//   var _add = function (thing) {
-//     $localStorage.things.push(thing);
-//   };
-//   var _remove = function (thing) {
-//     $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
-//   };
-//   var _setUser = function (user) {
-//     $localStorage.user = user;
-//   };
-//   var _getUser = function () {
-//     return $localStorage.user;
-//   };
-//   var _removeUser = function () {
-//     $localStorage.user = null;
-//   };
-//   return {
-//     getAll: _getAll,
-//     add: _add,
-//     remove: _remove,
-//     setUser: _setUser,
-//     getUser: _getUser,
-//     removeUser: _removeUser
-//   };
-// });
--- app/main/services/socket-serv.js
+++ app/main/services/socket-serv.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('main')
 
-.factory('Socket', ['$q', '$rootScope', function ($q, $rootScope) {
+.factory('Socket', ['$log', '$q', '$rootScope', '$ionicLoading', function ($log, $q, $rootScope) {
   // We return this object to anything injecting our service
   var Service = {};
   // Keep all pending requests here until they get responses
@@ -10,20 +10,51 @@ angular.module('main')
   var currentCallbackId = 0;
 
   // Create our websocket object with the address to the websocket
-  var ws = new WebSocket('ws://az0181d.abbvienet.com/AbbVie.Corp.Cop.RapidflowNG/WSService.svc');
+  // var ws = new WebSocket('ws://az0181d.abbvienet.com/AbbVie.Corp.Cop.RapidflowNG/WSService.svc');
 
-  ws.onopen = function () {
-    console.log('Socket has been opened!');
-  };
+  // ws.onopen = function () {
+  //   $log.log('Socket has been opened!');
+  // };
 
-  ws.onclose = function () {
-    console.log('Socket has been closed!');
+  // ws.onclose = function () {
+  //   $log.log('Socket has been closed!');
+  //   ws = new WebSocket('ws://az0181d.abbvienet.com/AbbVie.Corp.Cop.RapidflowNG/WSService.svc');
+  // };
+
+  // ws.onmessage = function (message) {
+  //   listener(JSON.parse(message.data));
+  // };
+  var ws = null;
+
+  function start () {
     ws = new WebSocket('ws://az0181d.abbvienet.com/AbbVie.Corp.Cop.RapidflowNG/WSService.svc');
-  };
+    ws.onopen = function () {
+      console.log('connected!');
+    };
+    ws.onmessage = function (message) {
+      listener(JSON.parse(message.data));
+    };
+    ws.onclose = function () {
+      console.log('closed!');
+      //reconnect now
+      check();
+    };
+    ws.onerror = function () {
+      alert('Socket Error No VPN');
+    };
 
-  ws.onmessage = function (message) {
-    listener(JSON.parse(message.data));
-  };
+  }
+
+  function check () {
+    if (!ws || ws.readyState === 3)
+    {
+      start();
+    }
+  }
+
+  start();
+
+  setInterval(check, 5000);
 
   function sendRequest (request) {
     var defer = $q.defer();
@@ -33,19 +64,24 @@ angular.module('main')
       cb: defer
     };
     request.callBackId = callbackId;
-    console.log('Sending request', request);
-    ws.send(JSON.stringify(request));
+    $log.log('Sending request', request);
+    /////////////////////////////////////////
+    waitForSocketConnection(ws, function () {
+      console.log('message sent!!!');
+      ws.send(JSON.stringify(request));
+    });
+    //ws.send(JSON.stringify(request));
     return defer.promise;
   }
 
   function listener (data) {
     var messageObj = data[0];
-    console.log('Received data from websocket: ', messageObj);
+    $log.log('Received data from websocket: ', messageObj);
     //If an object exists with callback_id in our callbacks object, resolve it
 
     if (callbacks.hasOwnProperty(messageObj.callBackId))
     {
-      console.log(callbacks[messageObj.callBackId]);
+      $log.log(callbacks[messageObj.callBackId]);
       //$rootScope.$apply(callbacks[1].cb.resolve(messageObj)); // hardcod ed now server not returning the callbackID...
       $rootScope.$apply(callbacks[messageObj.callBackId].cb.resolve(messageObj.data));
       delete callbacks[messageObj.callBackId];
@@ -61,23 +97,55 @@ angular.module('main')
     return currentCallbackId;
   }
 
-  // Socket Methods to be used by the rest of the Controllers/Services to make a specific call to Server
-  Service.authenticate = function (username, password) {
+  // // Socket Methods to be used by the rest of the Controllers/Services to make a specific call to Server
+  // Service.authenticate = function (username, password) {
 
-    var methodName = 'AuthenticateUser';
+  //   var methodName = 'AuthenticateUser';
 
-    var credentials = new Object();
-    credentials.userName = username;
-    credentials.password = password;
+  //   var credentials = new Object();
+  //   credentials.userName = username;
+  //   credentials.password = password;
 
+  //   var request = {
+  //     methodName: methodName,
+  //     parameterObj: JSON.stringify(credentials),
+  //   };
+  //   // Storing in a variable for clarity on what sendRequest returns
+  //   var promise = sendRequest(request);
+  //   return promise;
+  // };
+
+  Service.callWebSocketService = function (methodName, parameterObj) {
     var request = {
       methodName: methodName,
-      parameterObj: JSON.stringify(credentials),
+      parameterObj: JSON.stringify(parameterObj)
     };
     // Storing in a variable for clarity on what sendRequest returns
     var promise = sendRequest(request);
     return promise;
   };
+  Service.returnsocket = function () {
+    return ws;
+  };
+
+  // Make the function wait until the connection is made...
+  function waitForSocketConnection (socket, callback) {
+    setTimeout(
+      function () {
+        if (socket.readyState === 1) {
+          console.log('Connection is made');
+          if (callback !== null) {
+            callback();
+          }
+          return;
+        }
+        else {
+          console.log('wait for connection...');
+          waitForSocketConnection(socket, callback);
+        }
+
+      }, 1000); // wait 5 milisecond for the connection...
+  }
 
   return Service;
 }]);
