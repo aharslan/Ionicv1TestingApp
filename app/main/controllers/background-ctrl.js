b89e4d84d87d81a65a8a3d460f37c5baedf8aa9b
Ahmad Arslan <arslan0157@gmail.com>
2017-02-01 13:07:29 +0500

    added counter

--- app/main/controllers/background-ctrl.js
+++ app/main/controllers/background-ctrl.js
@@ -1,7 +1,49 @@
 'use strict';
 angular.module('main')
-.controller('BackgroundCtrl', function ($log) {
+.controller('BackgroundCtrl', function ($log, $localStorage, $scope, $timeout) {
 
-  $log.log('Hello from your Controller: BackgroundCtrl in module main:. This is your controller:', this);
+  $scope.count = 0;
+  $localStorage.count = 0;
+
+  var updateCounter = function () {
+    $localStorage.count ++;
+    $scope.count = $localStorage.count;
+    $timeout(updateCounter, 1000);
+  };
+  updateCounter();
+
+  $scope.reset = function () {
+    $scope.count = 0;
+    $localStorage.count = 0;
+  };
 
 });
+
+// .factory('StorageService', function ($localStorage) {
+//   var _getAll = function () {
+//     return $localStorage.things;
+//   };
+//   var _add = function (thing) {
+//     $localStorage.things.push(thing);
+//   };
+//   var _remove = function (thing) {
+//     $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
+//   };
+//   var _setUser = function (user) {
+//     $localStorage.user = user;
+//   };
+//   var _getUser = function () {
+//     return $localStorage.user;
+//   };
+//   var _removeUser = function () {
+//     $localStorage.user = null;
+//   };
+//   return {
+//     getAll: _getAll,
+//     add: _add,
+//     remove: _remove,
+//     setUser: _setUser,
+//     getUser: _getUser,
+//     removeUser: _removeUser
+//   };
+// });
--- app/main/main.js
+++ app/main/main.js
@@ -3,6 +3,7 @@ angular.module('main', [
   'ionic',
   'ngCordova',
   'ui.router',
+  'ngStorage',
   // TODO: load other modules selected during generation
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
